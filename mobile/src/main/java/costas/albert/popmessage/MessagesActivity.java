010eb7ffd565c811896e770c47e2427eb2f75cd1
Albert Costas <acostasg@uoc.edu>
2017-04-30 23:16:03 +0200

    Add activity to print messages

---
+++ mobile/src/main/java/costas/albert/popmessage/MessagesActivity.java
@@ -0,0 +1,37 @@
+package costas.albert.popmessage;
+
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import costas.albert.popmessage.Listener.PolicyCheckedListener;
+
+public class MessagesActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+}
