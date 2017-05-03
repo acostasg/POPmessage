7f000d4b565c25ee8f60cc04a78a40cc351b1722
Albert Costas <acostasg@uoc.edu>
2017-05-03 06:11:22 +0200

    Extract to logic to service create list message, and change view to component to view register

--- mobile/src/main/java/costas/albert/popmessage/MessagesActivity.java
+++ mobile/src/main/java/costas/albert/popmessage/MessagesActivity.java
@@ -1,25 +1,36 @@
 package costas.albert.popmessage;
 
-import android.app.Activity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
-import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
+import costas.albert.popmessage.services.ListMessagesService;
 import costas.albert.popmessage.task.UserLogOutTask;
 
 public class MessagesActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private final ListMessagesService listMessagesService = new ListMessagesService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+
+        FloatingActionButton newMessage = (FloatingActionButton) this.findViewById(R.id.new_message);
+        newMessage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+        listMessagesService.initListMessages(this, R.id.messages);
     }
 
     @Override
@@ -31,7 +42,7 @@ public class MessagesActivity extends AppCompatActivity implements LoaderManager
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         switch (id) {
             case R.id.log_out:
---
+++ mobile/src/main/java/costas/albert/popmessage/services/ListMessagesService.java
@@ -0,0 +1,86 @@
+package costas.albert.popmessage.services;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import costas.albert.popmessage.MessagesActivity;
+
+public class ListMessagesService {
+    public ListMessagesService() {
+    }
+
+    public void initListMessages(MessagesActivity messagesActivity, int id) {
+        final ListView listview = (ListView) messagesActivity.findViewById(id);
+        //TODO create task async to get api messages
+        String[] values = new String[]{"Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(
+                messagesActivity,
+                android.R.layout.simple_list_item_1,
+                list
+        );
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(2000).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                list.remove(item);
+                                adapter.notifyDataSetChanged();
+                                view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
+    }
+
+    class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        StableArrayAdapter(
+                Context context,
+                int textViewResourceId,
+                List<String> objects
+        ) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+}
